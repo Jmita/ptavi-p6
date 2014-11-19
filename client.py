083c8ca032605dae8f22ae584b44197a59b351a2
Juan Manuel Yuste Carretero <juanmis@iota17.aulas.gsyc.es>
2014-11-19 18:40:22 +0100

    client.py **

--- client.py
+++ client.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
 
 import socket
+import sys
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+# Datos introducidos
+metodo = sys.argv[1]
+print metodo
+division = sys.argv[2].split("@")
+nick = division[0]
+division2 = division[1].split(":")
+IP = division2[0]
+PORT = int(division2[1])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+if metodo == 'INVITE':
+    line = "INVITE sip:" + nick + "@" + IP + " SIP/2.0"
+elif metodo == 'BYE':
+    line = "BYE sip:" + nick + "@" + IP + " SIP/2.0"
+else:
+    print "Usage: python cliente.py method receiver@IP:SIPport"
+    sys.exit()
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+print "Enviando: " + line
+my_socket.send(line + '\r\n')
 
-# Cerramos todo
+try:
+    data = my_socket.recv(1024)
+    info = data.split("\r\n")
+    print "Recibido -- " + data
+
+except:
+    print "Fallo de recepcion"
+
 my_socket.close()
-print "Fin."
+print "Terminando socket..."
